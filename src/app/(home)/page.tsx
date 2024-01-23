378434ebe7ca4df2ca8cf2f8076495f1863d8f45
makkenzo <nekgo2009@gmail.com>
2024-01-23 16:33:39 +0600

    Add new dependencies and styles

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,10 +1,15 @@
+import { Button } from "@/components/ui/button";
 import { UserButton } from "@clerk/nextjs";
-import Link from "next/link";
 
 export default function HomePage() {
     return (
         <div>
             This is public route <UserButton afterSignOutUrl="/" />
+            <div className="max-w-[220px] mx-auto border-2 p-4 text-center space-y-4">
+                <Button variant="default">primary default</Button>
+                <Button variant="inactive">primary inactive</Button>
+                <Button variant="destructive">primary destructive</Button>
+            </div>
         </div>
     );
 }
